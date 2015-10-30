b825cbb2973cb80fa96ac3436f24e076dd722a11
Jeff Saul <scaleup13@gmail.com>
2015-10-29 21:48:40 -0600

    Added .gitignore for  bootstrap-wireframe project

---
+++ public_html/indes.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html land="en">
+	<head>
+		<meta charset="uff-8"/>
+		<title>Saul Bootstrap Wireframe Program</title>
+	</head>
+	<body>
+		//This is my Bootstrap Wireframe Project
+	</body>
+</html>
